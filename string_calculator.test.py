24e8af76eb4c7e81129f7c7f0dabdcb43bdfa4d5
Siddhant Kumar <siddhant18101@iiitnr.edu.in>
2022-09-09 15:43:34 +0530

    Update string_calculator.test.py

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -1,1 +1,9 @@
+import unittest
+import typewise_alert
 
+class StringCalculatorTest(unittest.TestCase):
+  def given_empty_string_0_expected(self):
+    self.assertTrue(string_calculator.add("") == 0)
+
+if __name__ == '__main__':
+  unittest.main()
